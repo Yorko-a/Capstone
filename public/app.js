cc5f3ac27653297eec552fd8f1c7046e43edc087
Jon <aljonjames1@gmail.com>
2024-12-14 18:20:22 +0800

    added email verification

--- public/app.js
+++ public/app.js
@@ -1,7 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js';
-import { getFirestore, doc, setDoc, query, where, getDocs, collection } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js';
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  sendEmailVerification
+ } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js';
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  query, 
+  where, 
+  getDocs, 
+  collection,
+} from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -53,6 +66,14 @@ if (signupForm) {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
 
+      // send email address verification
+      sendEmailVerification(user).then(() =>{
+        console.log('Email sent to:', email)
+        // if email sent
+      }).catch((error) =>{
+        console.error('Error email verification: ', error);
+      })
+
       // Store the username in Firestore
       await setDoc(doc(db, "users", user.uid), {
         username: userName,
@@ -65,6 +86,7 @@ if (signupForm) {
 
       // Clear the form
       signupForm.reset();
+      window.location.href = "/login.html"; // Redirect to login page
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -106,10 +128,18 @@ loginForm.addEventListener('submit', (e) => {
           signInWithEmailAndPassword(auth, emaoruse, password).then(
             async(userCredential) => {
               const user = userCredential.user;
-              console.log('User logged in successfully:', user);
-              alert('Logged in successfully!');
-
-              // redirect?...
+              
+              //is email verified?
+              if (user.emailVerified) {
+                console.log("Email is verified");
+                console.log('User logged in successfully:', user);
+                alert('Logged in successfully!');
+                // Redirect...
+              } else {
+                console.log("Email not verified");
+                alert("Please verify your email before logging in.");
+                auth.signOut(); // Sign the user out
+              }
             }).catch((error) => {
               const errorCode = error.code;
               const errorMessage = error.message;
@@ -132,12 +162,19 @@ loginForm.addEventListener('submit', (e) => {
       .then(async (userCredential) => {
         const user = userCredential.user;
         
-        console.log('User logged in successfully:', user);
-        alert('Logged in successfully!');
+        //is email verified?
+        if (user.emailVerified) {
+          console.log("Email is verified");
+          console.log('User logged in successfully:', user);
+          alert('Logged in successfully!');
         
-        // Redirect to another page or dashboard (optional)
-        // window.location.href = 'dashboard.html'; // Example
-
+        // Redirect to another page or dashboard
+          // Redirect user to the application
+        } else {
+          console.log("Email not verified");
+          alert("Please verify your email before logging in.");
+          auth.signOut(); // Sign the user out
+        }
       })
       .catch((error) => {
         const errorCode = error.code;
